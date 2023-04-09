4f8bca83e46d9a2e2c45afa115985bf917f2d48d
Vhyrro <vhyrro@gmail.com>
2023-04-09 17:35:35 +0200

    perf: don't create many `norg` languages

--- src/breeze.rs
+++ src/breeze.rs
@@ -1,23 +1,23 @@
+// TODO: Generalize to work with any language
+
 use anyhow::{anyhow, Result};
 use rusty_pool::Builder;
 use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
-use tree_sitter::Tree;
+use tree_sitter::{Tree, Language};
 
 /// Parses a file and returns its [`Tree`].
 ///
 /// * `filepath`: The path of the file to read.
-fn parse_file(filepath: &std::path::PathBuf) -> Result<Tree> {
+fn parse_file(filepath: &std::path::PathBuf, language: Language) -> Result<Tree> {
     let mut file = File::open(filepath)?;
 
-    // TODO: When a file exceeds a certain size, stream the file in block-by-block with
-    // parse_with()?
     let mut content = String::new();
     file.read_to_string(&mut content)?;
 
     let mut parser = tree_sitter::Parser::new();
-    parser.set_language(tree_sitter_norg::language())?;
+    parser.set_language(language)?;
 
     parser.parse(content, None).ok_or_else(|| {
         anyhow!(format!(
@@ -35,14 +35,18 @@ pub fn parse_files(files: Vec<PathBuf>) -> Result<Vec<Tree>> {
 
     let (tx, rx) = crossbeam_channel::bounded(file_count);
 
+    let language = tree_sitter_norg::language();
+
     for (i, file) in files.into_iter().enumerate() {
         let tx_clone = tx.clone();
 
         threadpool.execute(move || {
-            let parsed = parse_file(&file);
+            let parsed = parse_file(&file, language.clone());
             tx_clone.send((i, parsed)).unwrap();
         });
     }
+
+    threadpool.shutdown_join();
 
     for _ in 0..file_count {
         match rx.recv()? {
@@ -63,7 +67,7 @@ mod tests {
     #[test]
     fn test_parse_file() {
         let filepath = PathBuf::from("test/example_workspace/file1.norg");
-        let tree = parse_file(&filepath).unwrap();
+        let tree = parse_file(&filepath, tree_sitter_norg::language()).unwrap();
 
         assert!(tree.root_node().kind() == "document");
     }
